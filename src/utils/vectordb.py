1dcf5fdb5df5dddc7907a7ad264dc35f4feb7292
Mike Fischer <mike@theelectricrambler.com>
2024-06-01 09:06:19 +0200

    first attempt to move to chroma

--- src/utils/vectordb.py
+++ src/utils/vectordb.py
@@ -1,26 +1,32 @@
-# Helper Functions for Medium API Ingestion
-import weaviate
+import chromadb
 from llama_index.core import ServiceContext
 from llama_index.core import VectorStoreIndex
-from llama_index.vector_stores.weaviate import WeaviateVectorStore
-
-def get_weaviate_vector_store(client: weaviate.Client, index_name: str) -> WeaviateVectorStore:
-  vector_store = WeaviateVectorStore(weaviate_client=client, index_name=index_name)
-  return vector_store
+from llama_index.vector_stores.chromadb import ChromaDBVectorStore
 
 
-def get_index_from_weaviate_vector_store(vector_store: WeaviateVectorStore, service_context: ServiceContext) -> VectorStoreIndex:
+def get_chromadb_vector_store(
+    client: chromadb.Client, collection_name: str
+) -> ChromaDBVectorStore:
+    vector_store = ChromaDBVectorStore(
+        chroma_client=client, collection_name=collection_name
+    )
+    return vector_store
+
+
+def get_index_from_chromadb_vector_store(
+    vector_store: ChromaDBVectorStore, service_context: ServiceContext
+) -> VectorStoreIndex:
     index = VectorStoreIndex.from_vector_store(
-      vector_store=vector_store, service_context=service_context)
+        vector_store=vector_store, service_context=service_context
+    )
     return index
 
 
-def refresh_weaviate_index(documents: list, index: VectorStoreIndex) -> bool:
-   try:
-    index.refresh_ref_docs(documents)
-    status = True
-   except Exception:
-    status = False
-   finally:
-     return status
-   
+def refresh_chromadb_index(documents: list, index: VectorStoreIndex) -> bool:
+    try:
+        index.refresh_ref_docs(documents)
+        status = True
+    except Exception:
+        status = False
+    finally:
+        return status
